cf9c3ae86e01d333117389c5d78922419d0edea7
rebettendorf <ren.bettendorf@karlstorz.com>
2017-01-24 09:30:24 -0800

    Added initial protocols as an ENUM. Protocols are Message_Type in assignment

--- src/cs455/overlay/wireformats/Protocols.java
+++ src/cs455/overlay/wireformats/Protocols.java
@@ -1,5 +1,15 @@
 package cs455.overlay.wireformats;
 
-public class Protocols {
+public enum PROTOCOLS {
+	REGISTER_REQUEST(0), REGISTER_RESPONSE(1), DEREGISTER_REQUEST(2), DEREGISTER_RESPONSE(3), MESSAGING_NODES_LIST(4), 
+	LINK_WEIGHTS(5), TASK_INITIATE(6), TASK_COMPLETE(7), PULL_TRAFFIC_SUMMARY(8), TRAFFIC_SUMMARY(9);
 	
+	private int id;
+	PROTOCOLS(int id) {
+		this.id = id;
+	}
+	
+	public int getID() {
+		return id;
+	}
 }
