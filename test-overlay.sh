65a7a9ef2479c3517c4a6abe7571b130652bf0ac
rebettendorf <ren.bettendorf@karlstorz.com>
2017-02-15 16:11:23 -0800

    Removed a couple files that weren't necessary

--- test-overlay.sh
+++
@@ -1,7 +0,0 @@
-test_home=DijkstraNetworkAssignment/src
-
-for i in `cat machine_list`
-	do
-		echo 'logging into '${i}
-		gnome-terminal -x bash -c "ssh -t ${i} 'cd ${test_home}; java cs455.overlay.node.MessagingNode 129.82.44.156 2000;bash;'" &
-	done
